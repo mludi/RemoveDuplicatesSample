8db0420be4754adf01329a5e0e63c9e777423e61
Matthias Ludwig <ml@mypromo.com>
2021-01-27 14:25:30 +0100

    Initial Commit

---
+++ RemoveDuplicatesSample/RemoveDuplicatesSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemoveDuplicatesSampleApp.swift
+//  RemoveDuplicatesSample
+//
+//  Created by Matthias Ludwig on 27.01.21.
+//
+
+import SwiftUI
+
+@main
+struct RemoveDuplicatesSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
